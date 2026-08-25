eedd669843e858edbe58341313389a64135e1b7b
sandeepjais09 <sandeep.j.jaiswal@ericsson.com>
2026-08-25 09:00:50 +0000

    Fix variable declaration for activitySelect length reset

--- src/static/app.js
+++ src/static/app.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       // Clear loading message
       activitiesList.innerHTML = "";
-      activitySelect.length = 1;
+      const newLocal = activitySelect.length = 1;
 
       // Populate activities list
       Object.entries(activities).forEach(([name, details]) => {
